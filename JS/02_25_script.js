44eb4c9b5f2b79413198d7a907df0c093f12cc8d
Ritam Sanyal <ritamsanyal22@gmail.com>
2024-04-17 17:31:51 +0530

    modified

--- JS/02_25_script.js
+++ JS/02_25_script.js
@@ -6,3 +6,11 @@ var c = "Ritam";
 console.log(a + b + 8)
 console.log(typeof a, typeof b, typeof c)
 // test
+let x = "ritam";
+let y = 22;
+let z = 3.55;
+const p = true;
+let q = undefined;
+let r = null;
+console.log(x, y, z, c, q, r);
+console.log(typeof x, typeof y, typeof z, typeof p , typeof q, typeof r);
