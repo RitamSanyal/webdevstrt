b69260dfa93522968e67ba6deec659a6b37aefdb
Ritam Sanyal <ritamsanyal22@gmail.com>
2024-04-24 03:35:35 +0530

    new file

---
+++ JS/03_script.js
@@ -0,0 +1,13 @@
+console.log("This is conditional test")
+
+let age = 17
+let grace = 2
+
+if((age+grace)>=18)
+{
+    console.log("You Can Drive")
+}
+else
+{
+    console.log("You can't drive")
+}
