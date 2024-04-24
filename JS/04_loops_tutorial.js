65a576457364bf47a03b3bb856d6ba3ffc261b42
Ritam Sanyal <ritamsanyal22@gmail.com>
2024-04-24 15:51:44 +0530

    minor changes

---
+++ JS/04_loops_tutorial.js
@@ -0,0 +1,31 @@
+console.log("Looping Tutorial")
+
+let a = 1;
+for (let i = 0; i < 100; i++) {
+    console.log(a+i);
+}
+
+let obj = {
+    name: "Ritam Sanyal",
+    age: "22",
+    job: "web dev intern",
+    city: "Kolkata",
+    salary: "10LPA"
+}
+
+for (const key in obj) {
+    // if (Object.hasOwnProperty.call(obj, key)) {
+        const element = obj[key];
+        console.log(key,element)
+    // }
+}
+
+for (const c of "Ritam Sanyal") {
+    console.log(c)
+}
+
+let i = 0;
+while (i<=6) {
+    console.log(i);
+    i++;
+}
