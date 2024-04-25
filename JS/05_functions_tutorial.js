0a709d8c9b333c731fa4f7a8f15997587fa7b482
Ritam Sanyal <ritamsanyal22@gmail.com>
2024-04-25 14:11:26 +0530

    new file

---
+++ JS/05_functions_tutorial.js
@@ -0,0 +1,19 @@
+console.log("Function Tutorial");
+function nice(name) {
+    console.log("Hey " + name + " how are you doing");
+}
+
+nice("Ritam")
+nice("Arijit")
+
+function sum(a, b) {
+    // console.log(a+b);
+    return a + b;
+}
+
+var a = 2;
+var b = 3;
+
+result = sum(a, b);
+
+console.log("The sum of these numbers is : ", result)
