6dfde4b0d8f7c7b01836dba63d66b49b8fd8e9a0
Ritam Sanyal <ritamsanyal22@gmail.com>
2024-09-28 00:06:30 +0530

    modified

--- JS/09_express.js
+++ JS/09_express.js
@@ -1,17 +1,23 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+
+app.use(function(req,res,next){
+    console.log("1");
+    next();
+});
 
 app.get("/",function(req,res){
     res.send("Hello how are you Ritam Sanyal")
 
-})
+});
+
 app.get("/profile", function (req, res) {
     res.send("Welcome to the profile")
 
-})
+});
 
 const port = 3000;
 
 app.listen(port,()=>{
     console.log(`server is running on port ${port}`)
-})
+});
