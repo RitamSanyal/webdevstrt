7e37e734fc73732d5bb389a91b9a5e7bedb66645
Ritam Sanyal <ritamsanyal22@gmail.com>
2024-09-28 16:39:16 +0530

    Modified

--- Backend/script.js
+++ Backend/script.js
@@ -1,18 +1,25 @@
 const express = require('express');
 const app = express();
 
-app.use(function(req,res,next){
-    console.log('Executed');
-    next();
-});
+app.set("view engine", "ejs");
+app.use(express.static('./public'))
+
+// app.use(function(req,res,next){
+//     console.log('Executed');
+//     next();
+// });
 
 app.get('/', function (req, res) {
-    res.send("Hello World");
+    res.render("index")
 });
 
-app.get('/profile', function (req, res) {
-    res.send('Hello from profile')
+app.get('/profile/', function (req, res) {
+    res.render("profile")
 });
+
+// app.get('/profile/:username', function (req, res) {
+//     res.send(`Hello from ${req.params.username}'s profile`)
+// });
 
 app.listen(3000, () => {
     console.log("Server running successfully");
