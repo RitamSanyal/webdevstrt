9e0afac0b99619601a19162fb627c87aa1adb425
Ritam Sanyal <ritamsanyal22@gmail.com>
2024-10-04 02:25:12 +0530

    modified

--- testapp/app.js
+++ testapp/app.js
@@ -3,16 +3,22 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session  = require('express-session');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
-var app = express();
+var app = express(session({
+  resave: false,
+  saveUninitialized: false,
+  secret: 'G4K7dJ9bN8eRfV6cX5zA4sD3fG2hJ1kL'
+}));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use()
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
