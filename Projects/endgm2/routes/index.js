b2a104bdaaa5371bc256cd5572c136b51cf494b0
Ritam Sanyal <ritamsanyal22@gmail.com>
2024-10-25 02:51:59 +0530

    "Added express-session and connect-flash middleware, updated index route to use flash, and modified index.ejs view to use Bootstrap"

--- Projects/endgm2/routes/index.js
+++ Projects/endgm2/routes/index.js
@@ -2,8 +2,18 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res) {
+  res.render('index');
+});
+
+router.get('/failed', function (req, res) {
+  req.flash("age", 12);
+  res.send("Created Flash Value");
+});
+
+router.get('/check', function (req, res) {
+  console.log(req.flash("age"));
+  res.send("Check Console");
 });
 
 module.exports = router;
