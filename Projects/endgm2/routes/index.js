9d981f547206cd79adc9a21f62a04c53fcd87913
Ritam Sanyal <ritamsanyal22@gmail.com>
2024-10-26 02:41:35 +0530

    "Added Passport.js authentication, updated user model and routes, and created register and login forms in index.ejs"

--- Projects/endgm2/routes/index.js
+++ Projects/endgm2/routes/index.js
@@ -2,27 +2,68 @@ var express = require('express');
 var router = express.Router();
 const userModel = require("./users");
 const { search } = require('../app');
+const passport = require('passport');
+const localStrategy = require('passport-local');
+passport.use(new localStrategy(userModel.authenticate()));
 
 /* GET home page. */
 router.get('/', function (req, res) {
   res.render('index');
 });
 
-router.get('/create', async function (req, res) {
-  let userdata = await userModel.create({
-    username: "Ritam",
-    nickname: "Babi",
-    description: "I am a 22 year old guy",
-    categories: ['MERN', 'Problem Soving', 'Machine Learning'],
-  })
-  res.send(userdata);
+router.get('/profile', isLoggedIn ,function (req, res) {
+  res.render('profile');
 });
 
-router.get('/find', async function (req, res) {
-  var regexp = new RegExp("RITAM", "i")
-  let userdata = await userModel.find({ username: regexp });
-  res.send(userdata)
-})
+
+router.post('/register', function (req, res) {
+  var userdata = new userModel({
+    username: req.body.username,
+    secret: req.body.secret
+  });
+
+  userModel.register(userdata, req.body.password)
+    .then(function (registereduser) {
+      passport.authenticate("local")(req, res, function () {
+        res.redirect('/profile');
+      })
+    })
+});
+
+router.post('/login', passport.authenticate("local", {
+  successRedirect: "/profile",
+  failureRedirect: "/"
+}), function (req, res) { });
+
+router.get('/logout', function (req, res, next) {
+  req.logout(function (err) {
+    if (err) { return next(err); }
+    res.redirect('/');
+  });
+});
+
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect('/');
+}
+
+// router.get('/create', async function (req, res) {
+//   let userdata = await userModel.create({
+//     username: "Ritam",
+//     nickname: "Babi",
+//     description: "I am a 22 year old guy",
+//     categories: ['MERN', 'Problem Soving', 'Machine Learning'],
+//   })
+//   res.send(userdata);
+// });
+
+// router.get('/find', async function (req, res) {
+//   var regexp = new RegExp("RITAM", "i")
+//   let userdata = await userModel.find({ username: regexp });
+//   res.send(userdata)
+// })
 
 // router.get('/failed', function (req, res) {
 //   req.flash("age", 12);
--- Projects/endgm2/routes/users.js
+++ Projects/endgm2/routes/users.js
@@ -1,19 +1,13 @@
 const mongoose = require('mongoose');
-
+const plm = require('passport-local-mongoose')
 mongoose.connect("mongodb://127.0.0.1:27017/testingendgm2");
 
 const userSchema = mongoose.Schema({
   username: String,
-  nickname: String,
-  description: String,
-  categories: {
-    type: Array,
-    default: [],
-  },
-  date: {
-    type: Date,
-    default: Date.now,
-  }
+  password: String,
+  secret: String,
 });
+
+userSchema.plugin(plm);
 
 module.exports = mongoose.model("user", userSchema);
