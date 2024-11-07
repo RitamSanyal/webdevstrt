2f3d03c72c87566e55394571ad3df607505fad69
Ritam Sanyal <ritamsanyal22@gmail.com>
2024-11-08 01:10:28 +0530

    "Added passport.js authentication, updated routes and views for login and registration, and modified user model to include password"

--- Projects/ITS_Project/models/User.js
+++ Projects/ITS_Project/models/User.js
@@ -1,7 +1,12 @@
 const mongoose = require('mongoose');
+const plm = require('passport-local-mongoose')
 
 const UserSchema = new mongoose.Schema({
     user_id: {
+        type: String,
+        required: true
+    },
+    password: {
         type: String,
         required: true
     },
@@ -14,5 +19,7 @@ const UserSchema = new mongoose.Schema({
         default: Date.now
     }
 });
+
+UserSchema.plugin(plm);
 
 module.exports = mongoose.model('User', UserSchema);
--- Projects/ITS_Project/routes/index.js
+++ Projects/ITS_Project/routes/index.js
@@ -1,13 +1,16 @@
 var express = require('express');
 var router = express.Router();
 const User = require('../models/User')
+const passport = require('passport');
+const localStrategy = require('passport-local');
+passport.use(new localStrategy(User.authenticate()));
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
 });
 
-router.post('/select-stream', async (req, res) => {
+router.post('/select-stream', isLoggedIn ,async (req, res) => {
   try {
     const { user_id, stream_choice } = req.body;
     const newUser = new User({ user_id, stream_choice });
@@ -18,5 +21,37 @@ router.post('/select-stream', async (req, res) => {
     res.status(500).json({ error: 'Server error' });
   }
 });
+
+router.post('/register', function (req, res) {
+  var userdata = new userModel({
+    user_id: req.body.user_id
+  });
+
+  userModel.register(userdata, req.body.password)
+    .then(function (registereduser) {
+      passport.authenticate("local")(req, res, function () {
+        res.redirect('/profile');
+      })
+    })
+});
+
+router.post('/login', passport.authenticate("local", {
+  successRedirect: "/profile",
+  failureRedirect: "/"
+}), function (req, res) { });
+
+router.get('/logout', function (req, res, next) {
+  req.logout(function (err) {
+    if (err) { return next(err); }
+    res.redirect('/');
+  });
+});
+
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect('/');
+}
 
 module.exports = router;
