1be6dfd2354b1f7e4249939de87397d62b048810
Ritam Sanyal <ritamsanyal22@gmail.com>
2025-02-21 04:01:03 +0530

    modified

--- Projects/React_Test/02_react_basics/src/App.jsx
+++ Projects/React_Test/02_react_basics/src/App.jsx
@@ -1,9 +1,23 @@
-function App(){
-    return <div>
-        <h1>Hello From React App</h1>
-        <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. In provident modi et ab alias amet nesciunt fugiat recusandae ipsam quod? Debitis error ad adipisci libero ut aspernatur rem, odio mollitia.
-        </p>
-    </div>;
+import { useState } from "react"
+
+const App = () => {
+    const [a, setA] = useState("Ritam Sanyal")
+    const changeA = () => {
+        if (a === "Ritam Sanyal")
+            setA("Ankan Kunar Paul");
+        else
+            setA("Ritam Sanyal")
+    }
+    const [num, setnum] = useState(0)
+    return (
+        <div>
+            <h2 className="text-xl">Username is {a}</h2>
+            <button className="" onClick={changeA}>Change Username</button>
+            <p>Current value is {num}</p>
+            <button onClick={() => setnum(num + 1)}>Increment</button>
+            <button onClick={() => setnum(num - 1)}>Decrement</button>
+        </div>
+    )
 }
-export default App;
+
+export default App
