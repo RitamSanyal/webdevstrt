07945b44806c60268c1ca1ee4d545edd0d41b47a
Ritam Sanyal <ritamsanyal22@gmail.com>
2025-02-21 04:37:47 +0530

    `Removed App.css, updated App.jsx with new state variables and button functionality, and updated index.css with new styles.`

--- Projects/React_Test/02_react_basics/src/App.jsx
+++ Projects/React_Test/02_react_basics/src/App.jsx
@@ -2,20 +2,24 @@ import { useState } from "react"
 
 const App = () => {
     const [a, setA] = useState("Ritam Sanyal")
-    const changeA = () => {
+    const [num, setnum] = useState(0)
+    const [isDisabled, setIsDisabled] = useState(false);
+    const handleClick = () => {
         if (a === "Ritam Sanyal")
             setA("Ankan Kunar Paul");
         else
             setA("Ritam Sanyal")
-    }
-    const [num, setnum] = useState(0)
+        setIsDisabled(true);
+        setTimeout(() => setIsDisabled(false), 3000); // Enable after 3 seconds
+    };
     return (
         <div>
             <h2 className="text-xl">Username is {a}</h2>
-            <button className="" onClick={changeA}>Change Username</button>
+            <button onClick={handleClick} disabled={isDisabled} className={`py-2 px-4 font-bold rounded transition 
+        ${isDisabled ? "bg-gray-400 text-gray-700 cursor-not-allowed" : "bg-blue-500 hover:bg-blue-700 text-white"}`} >Change Username</button>
             <p>Current value is {num}</p>
-            <button onClick={() => setnum(num + 1)}>Increment</button>
-            <button onClick={() => setnum(num - 1)}>Decrement</button>
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mx-2 rounded focus:outline-none focus:ring-2 focus:ring-blue-300" onClick={() => setnum(num + 1)}>Increment</button>
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mx-2 rounded focus:outline-none focus:ring-2 focus:ring-blue-300" onClick={() => setnum(num - 1)}>Decrement</button>
         </div>
     )
 }
