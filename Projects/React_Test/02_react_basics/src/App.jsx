72b06886815380dd99ac9ad2348ffe71cc9851c0
Ritam Sanyal <ritamsanyal22@gmail.com>
2025-02-21 02:28:00 +0530

    `Removed unused imports and code, updated App component`

--- Projects/React_Test/02_react_basics/src/App.jsx
+++ Projects/React_Test/02_react_basics/src/App.jsx
@@ -1,35 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + (-5))}>
-          count is {count}
-        </button>
+function App(){
+    return <div>
+        <h1>Hello From React App</h1>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. In provident modi et ab alias amet nesciunt fugiat recusandae ipsam quod? Debitis error ad adipisci libero ut aspernatur rem, odio mollitia.
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>;
 }
-
-export default App
+export default App;
