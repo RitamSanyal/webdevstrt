a0811897f4d05442749e089660fdcd624605d3b8
Ritam Sanyal <ritamsanyal22@gmail.com>
2025-02-21 14:32:27 +0530

    `Added Tailwind CSS and updated styles, added form handling functionality to App component`

--- Projects/React_Test/03_react_form_handling/src/App.jsx
+++ Projects/React_Test/03_react_form_handling/src/App.jsx
@@ -1,9 +1,23 @@
+import { useState } from "react"
 const App = () => {
+  const [username, setusername] = useState('');
+  const submitHandler = (e) => {
+    e.preventDefault(),
+      console.log(username),
+      setusername('')
+  }
   return (
     <div>
-      <form>
-        <input type="text" />
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mx-2 rounded focus:outline-none focus:ring-2 focus:ring-blue-300">Submit</button>
+      <form onSubmit={(e) => {
+        submitHandler(e)
+      }}>
+        <input
+          value={username}
+          onChange={(e) => { setusername(e.target.value) }}
+          className="px-2 py-1 mx-2 rounded-sm" type="text"
+          placeholder="Enter your Name"
+        />
+        <button className="bg-emerald-600 hover:bg-blue-700 text-white font-bold py-2 px-4 mx-2 my-2 rounded focus:outline-none focus:ring-2 focus:ring-blue-300 ">Submit</button>
       </form>
     </div>
   )
