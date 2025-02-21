bc8196d5fbd8793eec4f9b7fb5f27f308930aa38
Ritam Sanyal <ritamsanyal22@gmail.com>
2025-02-21 16:10:25 +0530

    "Added Footer component to App.jsx"

---
+++ Projects/React_Test/04_react_components/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <>
+            <footer className="footer bg-emerald-800 fixed bottom-0 left-0 right-0 mx-auto px-1 py-1 text-center">2025 &copy; All rights reserved</footer>
+        </>
+    )
+}
+
+export default Footer
