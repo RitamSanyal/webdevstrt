7beee2686b0ee2575d63767bbeb8432689e8b562
Ritam Sanyal <ritamsanyal22@gmail.com>
2025-02-21 11:51:08 +0530

    modified

--- Projects/React_Test/03_react_form_handling/src/App.jsx
+++ Projects/React_Test/03_react_form_handling/src/App.jsx
@@ -1,11 +1,9 @@
-import React from 'react'
-
 const App = () => {
   return (
     <div>
       <form>
         <input type="text" />
-        <button>Submit</button>
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mx-2 rounded focus:outline-none focus:ring-2 focus:ring-blue-300">Submit</button>
       </form>
     </div>
   )
