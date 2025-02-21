df9d2f99dac19d7199e43974908ea5cacd4a104f
Ritam Sanyal <ritamsanyal22@gmail.com>
2025-02-22 00:44:19 +0530

    `Added import of Navbar_Part2 component and replaced hardcoded links with Navbar_Part2 in Navbar component`

---
+++ Projects/React_Test/04_react_components/src/components/Navbar_Part2.jsx
@@ -0,0 +1,12 @@
+const Navbar_Part2 = () => {
+  return (
+    <div className='flex gap-8 items-center'>
+      <h4 className='text-l font-semibold'>About</h4>
+      <h4 className='text-l font-semibold'>Contact</h4>
+      <h4 className='text-l font-semibold'>Services</h4>
+      <h4 className='text-l font-semibold'>Your Account</h4>
+    </div>
+  )
+}
+
+export default Navbar_Part2
