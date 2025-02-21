2f445c48be0bb533f1a800a4bbc01a3dfa6f1e91
Ritam Sanyal <ritamsanyal22@gmail.com>
2025-02-22 03:39:30 +0530

    `Update Card component button styles`

--- Projects/React_Test/04_react_components/src/components/Card.jsx
+++ Projects/React_Test/04_react_components/src/components/Card.jsx
@@ -6,7 +6,7 @@ const Card = (props) => {
             <h1 className="text-2xl font-semibold mb-4">{props.user} </h1>
             <h4 className="text-blue-600">{props.profession}</h4>
             <h2>City: {props.city}, Age: {props.age}</h2>
-            <button className="mt-5 bg-emerald-600 text-white px-4 py-2 rounded font-medium hover:bg-emerald-700 hover:scale-105 active:bg-emerald-800 active:scale-100 transition duration-200" onClick={() => alert('Friend added!')}>Add Friend</button>
+            <button className="mt-5 bg-emerald-600 text-white px-4 py-2 rounded font-medium hover:bg-emerald-700 hover:scale-105 active:bg-emerald-900 active:scale-100 transition duration-200" onClick={() => alert('Friend added!')}>Add Friend</button>
         </div>
     )
 }
