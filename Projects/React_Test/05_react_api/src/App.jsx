6c10fdad19db0876bf9c5c39ae3fd895071d12df
Ritam Sanyal <ritamsanyal22@gmail.com>
2025-02-23 01:49:00 +0530

    "Updated comments in App.jsx to reflect ref-1 reference consistency"

--- Projects/React_Test/05_react_api/src/App.jsx
+++ Projects/React_Test/05_react_api/src/App.jsx
@@ -10,7 +10,7 @@ const App = () => {
     setData(response.data);
   }
 
-  // * (ref 1) since we are using use effect button to call getData(),button is not required hence disabled
+  // * (ref-1) since we are using use effect button to call getData(),button is not required hence disabled
   useEffect(() => {
     getData()
   }, [])
@@ -18,7 +18,7 @@ const App = () => {
 
   return (
     <div className="p-10">
-      {/*// * this button is disabled (ref 1)  */}
+      {/*// * this button is disabled (ref-1)  */}
       {/* <button onClick={getData} className='bg-teal-700 text-white font-semibold text-2xl mb-2 px-6 py-3 rounded hover:bg-emerald-700 hover:scale-105 active:scale-95 transition duration-100'>Get Axios Data</button> */}
       <div className="p-5 mt-2 bg-gray-950">
         {data.map(function (elem, idx) {
