8fb5136a1f80cedd9fa8f78ebccaaffde03bfb37
Ritam Sanyal <ritamsanyal22@gmail.com>
2025-02-23 01:48:17 +0530

    `Added useEffect hook to call getData function on mount and removed button to get Axios data`

--- Projects/React_Test/05_react_api/src/App.jsx
+++ Projects/React_Test/05_react_api/src/App.jsx
@@ -1,5 +1,5 @@
 import axios from "axios"
-import {useState} from 'react'
+import { useState, useEffect } from 'react'
 import Card from "./components/Card";
 
 const App = () => {
@@ -8,15 +8,21 @@ const App = () => {
   const getData = async () => {
     const response = await axios.get('https://picsum.photos/v2/list');
     setData(response.data);
-    console.log(data)
   }
+
+  // * (ref 1) since we are using use effect button to call getData(),button is not required hence disabled
+  useEffect(() => {
+    getData()
+  }, [])
+
 
   return (
     <div className="p-10">
-      <button onClick={getData} className='bg-teal-700 text-white font-semibold text-2xl mb-2 px-6 py-3 rounded hover:bg-emerald-700 hover:scale-105 active:scale-95 transition duration-100'>Get Axios Data</button>
+      {/*// * this button is disabled (ref 1)  */}
+      {/* <button onClick={getData} className='bg-teal-700 text-white font-semibold text-2xl mb-2 px-6 py-3 rounded hover:bg-emerald-700 hover:scale-105 active:scale-95 transition duration-100'>Get Axios Data</button> */}
       <div className="p-5 mt-2 bg-gray-950">
-        {data.map(function(elem,idx){
-          return <Card key={idx} author={elem.author} download_url={elem.download_url}/>
+        {data.map(function (elem, idx) {
+          return <Card key={idx} author={elem.author} download_url={elem.download_url} />
         })}
       </div>
     </div>
