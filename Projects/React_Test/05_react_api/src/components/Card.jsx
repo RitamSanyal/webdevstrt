b4e700d7318bc69a8b98cf5a13bc568fc838701f
Ritam Sanyal <ritamsanyal22@gmail.com>
2025-02-23 00:44:27 +0530

    `Refactor Card component: update styles and layout`

--- Projects/React_Test/05_react_api/src/components/Card.jsx
+++ Projects/React_Test/05_react_api/src/components/Card.jsx
@@ -1,8 +1,8 @@
 const Card = (elem) => {
     return (
-        <div className="bg-gray-50 text-black flex items-center justify-between w-full px-7 py-6 rounded mb-2">
-            <img className="h-70 w-100 rounded" src={elem.download_url} alt="" />
-            <h1>{elem.author}</h1>
+        <div className="bg-gray-50 text-black inline-block items-center justify-between w-full px-7 py-6 rounded mb-2">
+            <img className="h-70 w-100 mr-1 rounded inline-block" src={elem.download_url} alt="" />
+            <h1 className="inline-block">{elem.author}</h1>
         </div>
     )
 }
