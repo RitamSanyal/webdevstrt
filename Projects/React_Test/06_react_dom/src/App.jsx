ae55b5619065ce4a81709df27488911c4d96d43b
Ritam Sanyal <ritamsanyal22@gmail.com>
2025-02-24 03:21:58 +0530

    `Added Header component and updated App component to include it`

--- Projects/React_Test/06_react_dom/src/App.jsx
+++ Projects/React_Test/06_react_dom/src/App.jsx
@@ -3,14 +3,16 @@ import About from "./pages/About";
 import Home from "./pages/Home";
 import Contact from "./pages/Contact";
 import Product from "./pages/Product";
+import Header from "./components/Header";
 const App = () => {
   return (
     <>
+      <Header />
       <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/about" element={<About />}/>
-        <Route path="/contact" element={<Contact/>}/>
-        <Route path="/product" element={<Product/>}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/product" element={<Product />} />
       </Routes>
     </>
   )
--- Projects/React_Test/06_react_dom/src/components/Header.jsx
+++ Projects/React_Test/06_react_dom/src/components/Header.jsx
@@ -1,11 +1,20 @@
+import { Link } from "react-router-dom"
+
 const Header = () => {
+    
     return (
-        <div className='py-7 px-10 bg-emerald-600 text-white flex items-center justify-between'>
+        <div className='py-7 px-10 bg-emerald-600 text-white flex items-center justify-between h-20'>
             <h2 className='text-2xl'>Ritam Sanyal</h2>
-            <div className='flex gap-10'>
+            <div className='flex gap-10 text-lg underline'>
+                <Link to='/'>Home</Link>
+                <Link to='/about'>About</Link>
+                <Link to='/contact'>Contact</Link>
+                <Link to='/product'>Product</Link>
+                {/*// * Anchor tags given below are not used as they are not SPGA compliant hence they are not used,instead we use Link from react-router-dom as give above */}
+                {/* <a className="text underline" href="/">Home</a>
                 <a className='text underline' href="/about">About</a>
                 <a className='text underline' href="/contact">Contact</a>
-                <a className='text underline' href="/product">Product</a>
+                <a className='text underline' href="/product">Product</a> */}
             </div>
         </div>
     )
