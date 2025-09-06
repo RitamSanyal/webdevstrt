85f9add1bdf360faa22270c47647843411d1f43b
Ritam Sanyal <ritamsanyal22@gmail.com>
2025-09-07 01:37:25 +0530

    refactor: improve code formatting and consistency in auth middleware and user routes

--- Projects/UBER/Backend/middlewares/auth.middleware.js
+++ Projects/UBER/Backend/middlewares/auth.middleware.js
@@ -4,11 +4,12 @@ const jwt = require('jsonwebtoken');
 
 module.exports.authUser = async (req, res, next) => {
     const token = req.cookies.token || req.headers.authorization?.split(' ')[1];
+    
     if (!token) {
         return res.status(401).json({ message: 'Access denied.' });
     }
 
-    const isBlacklisted = await userModel.findOne({token: token});
+    const isBlacklisted = await userModel.findOne({ token: token });
     if (isBlacklisted) {
         return res.status(401).json({ message: 'Please login again.' });
     }
