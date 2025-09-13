c02a640e7cb70259ded5a6b2c693abb3a18c9d45
Ritam Sanyal <ritamsanyal22@gmail.com>
2025-09-13 13:13:51 +0530

    feat: add user authentication flow with login, signup, logout, and protected routes; integrate axios for API calls

---
+++ Projects/UBER/Frontend/src/pages/UserProtectWrapper.jsx
@@ -0,0 +1,21 @@
+import { useEffect } from "react"
+import { useNavigate } from "react-router-dom"
+
+const UserProtectWrapper = ({ children }) => {
+    const token = localStorage.getItem('token')
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        if (!token) {
+            navigate('/login')
+        }
+    }, [navigate, token])
+
+    return (
+        <>
+            {children}
+        </>
+    )
+}
+
+export default UserProtectWrapper
--- Projects/UBER/Frontend/src/pages/UserSignup.jsx
+++ Projects/UBER/Frontend/src/pages/UserSignup.jsx
@@ -1,23 +1,38 @@
-import { useState } from "react"
-import { Link } from "react-router-dom"
+import React, { useState } from "react"
+import { Link, useNavigate } from "react-router-dom"
+import axios from "axios"
+import { UserDataContext } from "../context/UserDataContext"
 
 const UserSignup = () => {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
     const [firstName, setFirstName] = useState('')
     const [lastName, setLastName] = useState('')
-    const [userData, setUserData] = useState({})
 
-    const submitHandler = (e) => {
+    const navigate = useNavigate()
+
+    const { setUser } = React.useContext(UserDataContext)
+
+    const submitHandler = async (e) => {
         e.preventDefault()
-        setUserData({
-            fullName:{
-                firstName: firstName,
-                lastName: lastName,
+        const newUser = {
+            fullname: {
+                firstname: firstName,
+                lastname: lastName,
             },
             email: email,
             password: password
-        })
+        }
+
+        const resposnse = await axios.post(`${import.meta.env.VITE_BASE_URL}/users/register`, newUser)
+
+        if (resposnse.status === 201) {
+            const data = resposnse.data
+            setUser(data.user)
+            localStorage.setItem('token', data.token)
+            navigate('/home')
+        }
+
         setEmail('')
         setPassword('')
         setFirstName('')
