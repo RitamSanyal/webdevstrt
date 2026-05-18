228c11b70073d6d486235b0363c63dcd9b59c57a
Ritam Sanyal <ritamsanyal22@gmail.com>
2026-05-18 14:56:33 +0530

    Update package scripts to use Vite and add signup page component

---
+++ Projects/remote_blog/src/routes/auth/signup/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { authClient } from '$lib/auth-client';
+</script>
+
+<h1>Sign up</h1>
+
+<form action="">
+	<div class="row">
+		<label>
+			Email:
+			<input required type="email" id="email" />
+		</label>
+	</div>
+</form>
