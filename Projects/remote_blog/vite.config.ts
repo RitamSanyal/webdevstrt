81e83e1bd5ff34c3b1bef20d8955d4ce55264551
Ritam Sanyal <ritamsanyal22@gmail.com>
2026-05-18 14:18:34 +0530

    Refactor project structure to use SvelteKit, remove demo authentication routes, and update TypeScript configuration

--- Projects/remote_blog/vite.config.ts
+++ Projects/remote_blog/vite.config.ts
@@ -1,6 +1,15 @@
+import path from 'path';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	resolve: {
+		alias: [
+			{
+				find: '__SERVER__',
+				replacement: path.resolve(process.cwd(), '.svelte-kit/generated/server')
+			}
+		]
+	}
 });
